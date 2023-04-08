e66a0b4355e30303ad8be74bf25ef994c32c4cd4
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-04-08 12:43:37 +0100

    updated index style sheet and added tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+  {
+  id: 1,
+  text: 'Doctors Appointment',
+  day: 'Feb 5th at 2:30pm',
+  reminder: 'true',
+  },
+
+  {
+    id: 2,
+    text: 'School Meeting',
+    day: 'May 8th at 5:30pm',
+    reminder: 'true',
+  },
+
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'March 2nd at 3:00pm',
+    reminder: 'false',
+    }
+]
+
+const Tasks = () => {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
