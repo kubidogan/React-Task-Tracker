ecc1afb148ec6b9242389d0aa4bb1ea29abc2f3a
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-04-11 21:12:43 +0100

    fixed button toggle, needed to pass onClick prop to Button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Header from './components/Header'
+import Header from "./components/Header"
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import PropTypes from "prop-types"
 import Button from "./Button"
 
-const Header = ({ title, onAdd, showAdd, handleToggle }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  // const onClick = () => {
+  //   console.log("button clicked")
+  // }
   return (
     <header className= "header">
       <h1>{title}</h1>
