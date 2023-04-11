c418d3143c1ec6a5ee820f9719ef6fbd06082515
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-04-11 21:25:50 +0100

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import PropTypes from "prop-types"
 import Button from "./Button"
 
 const Header = ({ title, onAdd, showAdd }) => {
-  // const onClick = () => {
-  //   console.log("button clicked")
-  // }
+
   return (
     <header className= "header">
       <h1>{title}</h1>
