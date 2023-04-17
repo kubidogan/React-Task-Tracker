560d29ff9da281bcf9fc1cee8f038c8f30793a5f
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-04-17 22:30:29 +0100

    added styling to about page back button

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
 const About = () => {
   return (
-    <div>
+    <div className='about'>
       <h4>Version 1.0.0</h4>
       <Link to='/'>Go Back</Link>
     </div>
